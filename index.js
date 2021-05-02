0de51c3d495034475990281ec3af7269ee756231
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2021-05-02 11:23:27 +0600

    dispatching action and subscribe

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const redux = require('redux');
 
+//
 const initState = {
     num: 0
 }
 
+//
 const rootReducer = (state = initState, action) => {
+    if (action.type === 'INC_NUM') {
+        return {
+            ...state,
+            num: state.num + 1
+        }
+    }
+    if (action.type === 'ADD_NUM') {
+        return {
+            ...state,
+            num: state.num + action.a
+        }
+    }
     return state;
 }
 
+//
 const myStore = redux.createStore(rootReducer);
 console.log(myStore.getState());
+
+//Subscribe
+
+myStore.subscribe(() => {
+    console.log("[Subscribe]", myStore.getState());
+})
+
+//Dispatching Actions
+
+myStore.dispatch({
+    type: 'INC_NUM'
+})
+
+myStore.dispatch({
+    type: 'ADD_NUM',
+    a: 34
+})
